736666c7dc60b43f2050abab5726e9e257943eb4
igorseabra4 <igor.seabra@ccc.ufcg.edu.br>
2019-06-19 15:38:25 -0300

    adiciona inicio de prolog

---
+++ prolog/projeto.pl
@@ -0,0 +1,33 @@
+:- set_prolog_flag(verbose, silent).
+:- [mensagens].
+:- initialization(main).
+
+escreve_mensagem(NumeroMensagem) :-
+  get_mensagem(NumeroMensagem, M),
+  write(M).
+
+creditos :- halt.
+
+sair :- halt.
+
+start :- halt.
+
+menu :-
+  escreve_mensagem(11),
+  read(Escolha),
+  shell(clear),
+  (((((Escolha = 1) ->
+  start) ; 
+  (Escolha = 2) ->
+  creditos) ;
+  (Escolha = 3) ->
+  sair) ;
+  menu).
+
+init :-
+  escreve_mensagem(0),
+  menu.
+
+main :-
+  shell(clear),
+  init.
